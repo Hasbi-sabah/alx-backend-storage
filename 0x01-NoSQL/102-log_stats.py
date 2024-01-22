178da1d80fbf56ab5b448c9e1c4ec06ccbeffdbb
H. Sabah <sabahhasbi00@gmail.com>
2024-01-22 17:34:52 +0000

    Update: forgot print('IPs:')

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     print(nginx_logs.count_documents({"method": "GET", "path": "/status"}),
           "status check")
 
+    print('IPs:')
     pipe = [
             {'$group': {'_id': '$ip', 'count': {'$sum': 1}}},
             {'$sort': {'count': -1}},
