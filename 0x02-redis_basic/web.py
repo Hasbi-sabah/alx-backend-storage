3fe05b94d68b9620a1e13b5cdd37b5150488f4f9
H. Sabah <sabahhasbi00@gmail.com>
2024-01-24 20:27:43 +0000

    Code fix: all inside

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,12 +4,14 @@
 import requests
 import redis
 
-client = redis.Redis()
-client.set("count", 0)
 
 
 def get_page(url: str) -> str:
     """obtain the HTML content of a particular URL and returns it."""
-    client.incr("count", 1)
+    client = redis.Redis()
+    if not client.get("count"):
+        client.set("count", 1)
+    else:
+        client.incr("count", 1)
     client.expire("count", 10)
     return requests.get(url).text
