7df0a902d43e8bf1f2220424494c91c4a3112903
H. Sabah <sabahhasbi00@gmail.com>
2024-01-24 10:39:12 +0000

    Code fix: docstring, pycodestyle, flushdb()

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
+""" module for the class Cache """
 from redis import Redis
 from uuid import uuid4
 from typing import Union
 
+
 class Cache:
+    """class Cache"""
+
     def __init__(self):
+        """Initialize a Cache instance using Redis"""
         self._redis = Redis()
-        self._redis.flushdb
+        self._redis.flushdb()
 
     def store(self, data: Union(str, bytes, int, float)) -> str:
+        """Store data in the cache."""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
