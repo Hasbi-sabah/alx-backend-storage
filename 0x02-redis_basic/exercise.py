7d4c3983da6cd6ca1fc9e51114abeafba221c549
H. Sabah <sabahhasbi00@gmail.com>
2024-01-24 10:54:23 +0000

    Code fix: type-annotation

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ module for the class Cache """
-from redis import Redis
+import redis
 from uuid import uuid4
 from typing import Union
 
@@ -10,10 +10,10 @@ class Cache:
 
     def __init__(self):
         """Initialize a Cache instance using Redis"""
-        self._redis = Redis()
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union(str, bytes, int, float)) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in the cache."""
         key = str(uuid4())
         self._redis.set(key, data)
