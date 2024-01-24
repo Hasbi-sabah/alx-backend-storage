3f9116daa22d4dcc9da8d8b64ac7f4ce4ce0b424
H. Sabah <sabahhasbi00@gmail.com>
2024-01-24 14:39:59 +0000

    Update: task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,6 +19,23 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Decorator to track method calls and their inputs/outputs in Redis"""
+    input_key = method.__qualname__ + ":inputs"
+    output_key = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def wrapper(self: Any, *args: Any, **kwargs: Any) -> Any:
+        """Wrapper function that records input and output data in Redis lists."""
+        input_data = str(args)
+        self._redis.rpush(input_key, input_data)
+        output_data = method(self, input_data)
+        self._redis.rpush(output_key, output_data)
+        return output_data
+
+    return wrapper
+
+
 class Cache:
     """class Cache"""
 
@@ -28,6 +45,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in the cache."""
         key = str(uuid4())
