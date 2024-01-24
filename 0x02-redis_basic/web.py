1d30433dd673185e4ff0f76ad87ea7d0f9f0e6b3
H. Sabah <sabahhasbi00@gmail.com>
2024-01-24 20:29:42 +0000

    Code fix: "count:{url}"

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,9 +9,9 @@ import redis
 def get_page(url: str) -> str:
     """obtain the HTML content of a particular URL and returns it."""
     client = redis.Redis()
-    if not client.get("count"):
-        client.set("count", 1)
+    if not client.get("count:{}".format(url)):
+        client.set("count:{}".format(url), 1)
     else:
-        client.incr("count", 1)
-    client.expire("count", 10)
+        client.incr("count:{}".format(url), 1)
+    client.expire("count:{}".format(url), 10)
     return requests.get(url).text
